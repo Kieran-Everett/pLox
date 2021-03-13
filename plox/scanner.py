d159d172a33193e7ecc4eccd9a94b6a35b974c89
Kieran Everett <kieraneverett03@gmail.com>
2021-03-13 23:09:23 +0000

    Fixed scanToken and added operators

--- plox/scanner.py
+++ plox/scanner.py
@@ -13,6 +13,17 @@ class Scanner():
         self.current = 0
         self.line = 1
 
+        self.isAtEnd = False
+    
+    def match(self, expected):
+        if ( self.isAtEnd ):
+            return False
+        if ( self.source[current] != expected ):
+            return False
+        
+        self.current += 1
+        return True
+
     def addToken(self, type, literal=None): # Overloading is neat
 
         text = self.source[self.start:self.current]
@@ -20,32 +31,55 @@ class Scanner():
 
     def advance(self):
 
-        self.current = self.current + 1
+        self.current += 1
         return self.source[self.current - 1]
     
     def scanToken(self):
         
-        c = advance()
-        if c == '(':
-            addToken(tokenType.TokenType.LEFT_PAREN.name)
-        elif c == ')':
-            addToken(tokenType.TokenType.RIGHT_PAREN.name)
-        elif c == '{':
-            addToken(tokenType.TokenType.LEFT_BRACE.name)
-        elif c == '}':
-            addToken(tokenType.TokenType.RIGHT_BRACE.name)
-        elif c == ',':
-            addToken(tokenType.TokenType.COMMA.name)
-        elif c == '.':
-            addToken(tokenType.TokenType.DOT.name)
-        elif c == '-':
-            addToken(tokenType.TokenType.MINUS.name)
-        elif c == '+':
-            addToken(tokenType.TokenType.PLUS.name)
-        elif c == ';':
-            addToken(tokenType.TokenType.SEMICOLON.name)
-        elif c == '*':
-            addToken(tokenType.TokenType.STAR.name)
+        c = self.advance()
+
+        if ( c == '(' ):
+            self.addToken(tokenType.TokenType.LEFT_PAREN.name)
+        elif ( c == ')' ):
+            self.addToken(tokenType.TokenType.RIGHT_PAREN.name)
+        elif ( c == '{' ):
+            self.addToken(tokenType.TokenType.LEFT_BRACE.name)
+        elif ( c == '}' ):
+            self.addToken(tokenType.TokenType.RIGHT_BRACE.name)
+        elif ( c == ',' ):
+            self.addToken(tokenType.TokenType.COMMA.name)
+        elif ( c == '.' ):
+            self.addToken(tokenType.TokenType.DOT.name)
+        elif ( c == '-' ):
+            self.addToken(tokenType.TokenType.MINUS.name)
+        elif ( c == '+' ):
+            self.addToken(tokenType.TokenType.PLUS.name)
+        elif ( c == ';' ):
+            self.addToken(tokenType.TokenType.SEMICOLON.name)
+        elif ( c == '*' ):
+            self.addToken(tokenType.TokenType.STAR.name)
+
+        elif ( c == '!' ):
+            if ( match('=') ):
+                self.addToken(tokenType.TokenType.BANG_EQUAL.name)
+            else:
+                self.addToken(tokenType.TokenType.BANG.name)
+        elif ( c == '=' ):
+            if ( match('=') ):
+                self.addToken(tokenType.TokenType.EQUAL_EQUAL.name)
+            else:
+                self.addToken(tokenType.TokenType.EQUAL.name)
+        elif ( c == '<' ):
+            if ( match('=') ):
+                self.addToken(tokenType.TokenType.LESS_EQUAL.name)
+            else:
+                self.addToken(tokenType.TokenType.LESS.name)
+        elif ( c == '>' ):
+            if ( match('=') ):
+                self.addToken(tokenType.TokenType.GREATER_EQUAL.name)
+            else:
+                self.addToken(tokenType.TokenType.GREATER.name)
+
         else:
             pLox.Lox.error(line, "Unexpected character.")
     
@@ -54,7 +88,7 @@ class Scanner():
     
     def scanTokens(self):
         
-        while ( isAtEnd == False ):
+        while ( self.isAtEnd == False ):
             start = current
             self.scanToken()
         
